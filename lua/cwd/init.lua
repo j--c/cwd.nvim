e533627f7046ee38406880b0acff20e1c84b45b0
j--c <johnmichaelcollins@protonmail.com>
2024-10-25 10:28:48 -0400

    Re-written to make more functions pulic so that they can be re-used by other config.

--- lua/cwd/init.lua
+++ lua/cwd/init.lua
@@ -1,5 +1,6 @@
 local M = {}
 
+
 local init_cfg = function()
     return {
         prmpt_msg = 'Change directory to: ',
@@ -7,9 +8,9 @@ local init_cfg = function()
         cncl_msg = 'Operation cancelled!',
         err_msg = '  Error - Invalid directory!',
         cmplt_msg = '  Success - Directory changed to: ',
-        tilde = '~'
     }
 end
+
 
 local upd_cfg = function(cfg)
     if type(cfg) == 'table' then
@@ -21,54 +22,39 @@ local upd_cfg = function(cfg)
     end
 end
 
-local wd_prompt = function(cncl_code)
-    local opts = {
-        prompt = M.cfg.prmpt_msg,
-        default = M.cfg.dflt_prmpt_val,
+
+local dir_prompt = function(opts)
+    return vim.fn.input( {
+        prompt = opts.prmpt_msg,
+        default = opts.dflt_prmpt_val,
         completion = 'dir' ,
-        cancelreturn = cncl_code
-    }
-    return vim.fn.input(opts)
+        cancelreturn = opts.cncl_code
+    })
 end
 
-local get_wd = function()
-    local cncl_code = 'a;lskdjf;a'
-    local wd = wd_prompt(cncl_code)
-    if wd == cncl_code then
-        wd = nil
-        print(M.cfg.cncl_msg)
-    end
-    return wd
-end
-
-local edit_wd = function(new_wd)
-    local trm_wd = new_wd:gsub('%s+', '')
-    local wd = trm_wd:gsub(M.cfg.tilde, vim.env.HOME)
-    return wd
-end
-
-local is_dir = function(wd)
-    return (vim.fn.isdirectory(wd)) ~= 0
-end
 
 ------------------------ PUBLIC API ------------------------
 
+
+-- setup
 M.setup = function(cfg)
     M.cfg = init_cfg()
     upd_cfg(cfg)
 end
 
-M.cwd = function(new_wd, opts)
+
+-- cwd
+M.cwd = function(new_wd_path, opts)
     local chgd = false
     upd_cfg(opts)
-    if new_wd == nil then
-        new_wd = get_wd()
+    if new_wd_path == nil then
+        new_wd_path = M.get_dir_f_usr(M.cfg)
     end
-    if new_wd ~= nil then
-        local wd = edit_wd(new_wd)
-        if is_dir(wd) then
-            vim.api.nvim_set_current_dir(wd)
-            print(M.cfg.cmplt_msg .. '"' .. wd .. '"')
+    if new_wd_path ~= nil then
+        local clean_wd_path = M.clean_dir_path(new_wd_path)
+        if M.valid_dir_path(clean_wd_path) then
+            vim.api.nvim_set_current_dir(clean_wd_path)
+            print(M.cfg.cmplt_msg .. '"' .. clean_wd_path .. '"')
             chgd = true
         else
             print(M.cfg.err_msg)
@@ -77,21 +63,50 @@ M.cwd = function(new_wd, opts)
     return chgd
 end
 
-M.swd = function(swp_wd, opts)
-   local new_wd = M.prv_wd or swp_wd
+
+-- swd
+M.swd = function(swp_wd_path, opts)
     upd_cfg(opts)
-    if M.prv_wd == nil then
-        M.prv_wd = vim.fn.getcwd()
+    local new_wd_path = M.prv_wd_path or swp_wd_path
+    if M.prv_wd_path == nil then
+        M.prv_wd_path = vim.fn.getcwd()
     else
-        new_wd = M.prv_wd
-        M.prv_wd = nil
+        new_wd_path = M.prv_wd_path
+        M.prv_wd_path = nil
     end
-    local chgd = M.cwd(new_wd, opts)
+    local chgd = M.cwd(new_wd_path, opts)
     if chgd == false then
-        M.prv_wd = nil
+        M.prv_wd_path = nil
     end
     return chgd
 end
+
+
+-- get_dir_f_usr
+M.get_dir_f_usr = function(opts)
+    opts.cncl_code = 'a;lskdjf;a'
+    local dir = dir_prompt(opts)
+    if dir == opts.cncl_code then
+        dir = nil
+        print(opts.cncl_msg)
+    end
+    return dir
+end
+
+
+-- clean_dir_path
+M.clean_dir_path = function(dir_path)
+    local trm_dir_path = dir_path:gsub('%s+', '')
+    local clean_path = trm_dir_path:gsub('~', vim.env.HOME)
+    return clean_path
+end
+
+
+-- valid_dir_path
+M.valid_dir_path = function(dir_path)
+    return (vim.fn.isdirectory(dir_path)) ~= 0
+end
+
 
 ------------------------------------------------------------
 
